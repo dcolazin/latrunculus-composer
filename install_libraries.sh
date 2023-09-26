e06d8b00c26883fabe435d453cd240fe55bfc956
dcolazin <dcolazin@gmail.com>
2023-09-27 01:42:49 +0200

    removed xerces

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,9 +1,5 @@
-mvn install:install-file -Dfile=third_party_libraries/jar/jsyn_16_7_1.jar -DgroupId=org.rubato.jars -DartifactId=jsyn -Dversion=0.1.0-MAVEN -Dpackaging=jar
-
 mvn install:install-file -Dfile=third_party_libraries/jar/junit.jar -DgroupId=org.rubato.jars -DartifactId=junit -Dversion=0.1.0-MAVEN -Dpackaging=jar
 
 mvn install:install-file -Dfile=third_party_libraries/jar/LeapJava.jar -DgroupId=org.rubato.jars -DartifactId=LeapJava -Dversion=0.1.0-MAVEN -Dpackaging=jar
 
 mvn install:install-file -Dfile=third_party_libraries/jar/mt4j.jar -DgroupId=org.rubato.jars -DartifactId=mt4j -Dversion=0.1.0-MAVEN -Dpackaging=jar
-
-mvn install:install-file -Dfile=third_party_libraries/jar/xercesImpl.jar -DgroupId=org.rubato.jars -DartifactId=xercesImpl -Dversion=0.1.0-MAVEN -Dpackaging=jar
