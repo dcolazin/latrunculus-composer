f11c33faece152819fe908938e6bafe15eb568dc
dcolazin <dcolazin@gmail.com>
2023-09-27 01:16:01 +0200

    removed glulogic mt

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,5 +1,3 @@
-mvn install:install-file -Dfile=third_party_libraries/jar/GlulogicMT.jar -DgroupId=org.rubato.jars -DartifactId=GlulogicMT -Dversion=0.1.0-MAVEN -Dpackaging=jar
-
 mvn install:install-file -Dfile=third_party_libraries/jar/jportaudio.jar -DgroupId=org.rubato.jars -DartifactId=jportaudio -Dversion=0.1.0-MAVEN -Dpackaging=jar
 
 mvn install:install-file -Dfile=third_party_libraries/jar/jsyn_16_7_1.jar -DgroupId=org.rubato.jars -DartifactId=jsyn -Dversion=0.1.0-MAVEN -Dpackaging=jar
