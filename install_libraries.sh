4b71b730c837ff1dfd8d8f7381d23a744c57f396
dcolazin <dcolazin@gmail.com>
2023-09-27 01:25:28 +0200

    removed jportaudio

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,5 +1,3 @@
-mvn install:install-file -Dfile=third_party_libraries/jar/jportaudio.jar -DgroupId=org.rubato.jars -DartifactId=jportaudio -Dversion=0.1.0-MAVEN -Dpackaging=jar
-
 mvn install:install-file -Dfile=third_party_libraries/jar/jsyn_16_7_1.jar -DgroupId=org.rubato.jars -DartifactId=jsyn -Dversion=0.1.0-MAVEN -Dpackaging=jar
 
 mvn install:install-file -Dfile=third_party_libraries/jar/junit.jar -DgroupId=org.rubato.jars -DartifactId=junit -Dversion=0.1.0-MAVEN -Dpackaging=jar
